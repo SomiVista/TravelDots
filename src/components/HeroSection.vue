186830330ceb8e1ce2caf58199ec2e037c2de758
SomiVista <somayerohani06@gmail.com>
2025-01-06 14:02:08 +0300

    feature

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -4,9 +4,11 @@
       class="container mx-auto px-4 flex flex-col md:flex-row items-center relative"
     >
       <!-- Text Section -->
-      <div class="md:w-1/2">
-        <h2>Best Destinations around the world</h2>
-        <h1 class="text-4xl md:text-5xl font-bold text-blue-900">
+      <div class="md:w-1/2 z-10">
+        <h2 class="text-xl text-gray-700">
+          Best Destinations around the world
+        </h2>
+        <h1 class="text-4xl md:text-5xl font-bold text-blue-900 mt-2">
           Travel, enjoy <br />
           and live a new <br />
           and full life
@@ -30,15 +32,23 @@
         </div>
       </div>
       <!-- Image Section -->
-
-      <div class="plane up">
-        <img src="/public/assets/Icons/Plane.png" alt="plane" />
-      </div>
-      <div class="md:w-1/2 mt-8 md:mt-0 z-20">
-        <img src="/public/assets/Traveller.png" alt="Traveler" class="w-full" />
-      </div>
-      <div class="plane down">
-        <img src="/public/assets/Icons/Plane.png" alt="plane" />
+      <div class="md:w-1/2 mt-8 md:mt-0 relative z-20">
+        <img class="w-full" src="/public/assets/Traveller.png" alt="Traveler" />
+        <!-- Plane Animations -->
+        <div class="plane plane-up">
+          <img
+            class="w-12 md:w-16"
+            src="/public/assets/Icons/Plane.png"
+            alt="plane"
+          />
+        </div>
+        <div class="plane plane-down">
+          <img
+            class="w-12 md:w-16"
+            src="/public/assets/Icons/Plane.png"
+            alt="plane"
+          />
+        </div>
       </div>
     </div>
   </section>
@@ -47,25 +57,92 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.plane {
+.container {
   position: relative;
-  animation: plane 10s linear 0s infinite reverse;
-}
-.up {
-  top: -250px;
-  right: -300px;
-}
-.down {
-  top: -100px;
-  right: -100px;
 }
 
-@keyframes plane {
-  from {
-    right: -500px;
+.plane {
+  position: absolute;
+  top: 20%;
+  width: 50px;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+.plane img {
+  display: block;
+}
+
+.plane-up {
+  left: -10%;
+  animation-name: flyRightUp;
+  animation-duration: 25s;
+  animation-delay: 0s;
+}
+
+.plane-down {
+  left: -15%;
+  top: 40%;
+  animation-name: flyRightDown;
+  animation-duration: 30s;
+  animation-delay: 5s;
+  transform: rotate(45deg);
+}
+
+@keyframes flyRightUp {
+  0% {
+    transform: translate(0, 0) rotate(0deg);
+    opacity: 0;
   }
-  to {
-    right: 102%;
+  10% {
+    opacity: 1;
+  }
+  50% {
+    transform: translate(110vw, -50vh) rotate(360deg);
+    opacity: 1;
+  }
+  60% {
+    opacity: 0;
+  }
+  100% {
+    transform: translate(110vw, -50vh) rotate(360deg);
+    opacity: 0;
+  }
+}
+
+@keyframes flyRightDown {
+  0% {
+    transform: translate(0, 0) rotate(0deg);
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  50% {
+    transform: translate(110vw, 50vh) rotate(-360deg);
+    opacity: 1;
+  }
+  60% {
+    opacity: 0;
+  }
+  100% {
+    transform: translate(110vw, 50vh) rotate(-360deg);
+    opacity: 0;
+  }
+}
+
+/* Responsive Adjustments */
+@media (min-width: 768px) {
+  .plane {
+    width: 70px;
+  }
+
+  .plane-up {
+    animation-duration: 20s;
+  }
+
+  .plane-down {
+    animation-duration: 25s;
   }
 }
 </style>
